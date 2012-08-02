f2d91fd1892c3c86fbd25115aa49c9bc300ceab4
Steve Anton <anxuiz.nx@gmail.com>
2012-08-02 09:43:35 -0700

    Update the build.sh and update.sh scripts.

--- build.sh
+++ build.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
-rm -rf build
-mkdir build
-cd build
 
-# prepare Bukkit
-git clone git://github.com/Bukkit/Bukkit.git
-cd Bukkit
-# change pom.xml to tc.oc
-sed --in-place "s#<groupId>org.bukkit</groupId>#<groupId>tc.oc</groupId>#" pom.xml
-sed --in-place "s#<artifactId>bukkit</artifactId>#<artifactId>pgm-bukkit</artifactId>#" pom.xml
-sed --in-place "s#<name>Bukkit</name>#<name>PGM-Bukkit</name>#" pom.xml
-# apply patches
-git apply ../Bukkit/*
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# build.sh
+#
+# Checks out clean copies of Bukkit & CraftBukkit then applies the patches
+# specified in the Bukkit/ and CraftBukkit/ directories.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # directory script is in
+
+if [ ! -d "$DIR/build" ]; then
+    echo "You must run the ./update script first!"
+    exit 1
+fi
+
+function apply_patches {
+    project=$1
+    project_lower=`echo $project | tr '[A-Z]' '[a-z]'`
+
+    $DIR/checkout.sh $project
+
+    cd $DIR/build/$project
+    echo "Editing the pom.xml for $project..."
+    
+    # change pom.xml to tc.oc
+    sed --in-place "s#<groupId>org.bukkit</groupId>#<groupId>tc.oc</groupId>#" pom.xml
+    sed --in-place "s#<artifactId>$project_lower</artifactId>#<artifactId>pgm-$project_lower</artifactId>#" pom.xml
+    sed --in-place "s#<name>$project</name>#<name>PGM-$project</name>#" pom.xml
+    # apply patches
+    git apply $DIR/$project/*.patch
+}
+
+apply_patches Bukkit
+apply_patches CraftBukkit
--- update.sh
+++ update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# update.sh
+#
+# Updates the cloned version of Bukkit & CraftBukkit in the build/ directory,
+# or newly clones them if they do not already exist there.  This should be the
+# first script run when setting up the PGM-CraftBukkit build environment.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 if [ ! -d "build" ]; then
     mkdir build
 fi
@@ -12,13 +21,7 @@ function update {
     else
         git clone git://github.com/Bukkit/$1.git
     fi
-
-    echo "Switching to a staging branch..."
-    git checkout master # can't update a branch you are already on
-    git checkout -B staging origin/master --force
 }
 
 update Bukkit
 update CraftBukkit
-
-

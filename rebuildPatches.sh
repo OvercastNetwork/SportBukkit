523cb3e5d6bb77e5b286491af9e2b6c1348153b5
mrapple <tony@oc.tc>
2013-07-08 10:23:34 -0500

    And magically, rebuildPatches started working again

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -6,10 +6,8 @@ echo "Rebuilding patch files from current fork state..."
 function cleanupPatches {
     cd $1
     for patch in *.patch; do
-        echo $patch
         gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
         diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
-        echo $diffs
 
         testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
         if [ "x$testver" != "x" ]; then
@@ -31,7 +29,7 @@ function rebuildPatches {
     git format-patch --no-stat -N -o $basedir/$patches/ upstream/upstream
     cd $basedir
     git add $basedir/$patches
-    #cleanupPatches $basedir/$patches
+    cleanupPatches $basedir/$patches
     echo "  Patches saved for $what to $patches"
 }
 

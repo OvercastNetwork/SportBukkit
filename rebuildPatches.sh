16c1260e2d6f385e278b34dc411218951a47d250
mrapple <tony@oc.tc>
2013-07-08 10:21:23 -0500

    Add some spigot patches, temporarily disable clean patches
    Also, add 1.6.2 protocol support

--- rebuildPatches.sh
+++ rebuildPatches.sh
@@ -6,8 +6,10 @@ echo "Rebuilding patch files from current fork state..."
 function cleanupPatches {
     cd $1
     for patch in *.patch; do
+        echo $patch
         gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
         diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index)")
+        echo $diffs
 
         testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
         if [ "x$testver" != "x" ]; then
@@ -29,7 +31,7 @@ function rebuildPatches {
     git format-patch --no-stat -N -o $basedir/$patches/ upstream/upstream
     cd $basedir
     git add $basedir/$patches
-    cleanupPatches $basedir/$patches
+    #cleanupPatches $basedir/$patches
     echo "  Patches saved for $what to $patches"
 }
 

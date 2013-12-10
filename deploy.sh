f636589f287de59efdcfa7d6da9561a03c6d419d
Steve Anton <anxuiz.nx@gmail.com>
2013-12-09 21:24:58 -0800

    Update deployment information and add deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ORIG_PWD="$(pwd)"
+cd "$(dirname "$0")"
+
+function deploy {
+    target=build/$1
+
+    echo "  $1 deploying..."
+    cd $target
+
+    if !(mvn deploy); then
+        echo "  $1 failed to deploy"
+        cd "$ORIG_PWD"
+        exit $?
+    else
+        echo "  $1 deployed"
+    fi
+
+    cd ../..
+}
+
+deploy Bukkit
+deploy CraftBukkit
+
+cd "$ORIG_PWD"

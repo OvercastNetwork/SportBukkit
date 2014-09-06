d688b897a9420d076a628cafb220e5f393b82253
Jedediah Smith <jedediah@silencegreys.com>
2014-09-05 22:24:30 -0400

    Apply patches to current build branch, require clean work tree

--- applyPatches.sh
+++ applyPatches.sh
@@ -4,6 +4,34 @@ ORIG_PWD="$(pwd)"
 cd "$(dirname "$0")"
 echo "  Rebuilding Forked projects.... "
 
+function requireCleanWorkTree {
+    # Update the index
+    git update-index -q --ignore-submodules --refresh
+    err=0
+
+    # Disallow unstaged changes in the working tree
+    if ! git diff-files --quiet --ignore-submodules --
+    then
+        echo >&2 "cannot apply patches to $1: you have unstaged changes."
+        git diff-files --name-status -r --ignore-submodules -- >&2
+        err=1
+    fi
+
+    # Disallow uncommitted changes in the index
+    if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+    then
+        echo >&2 "cannot apply patches to $1: your index contains uncommitted changes."
+        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        echo >&2 "Please commit or stash them."
+        exit 1
+    fi
+}
+
 function applyPatches {
     what=base/$1
     target=build/$1
@@ -17,10 +45,12 @@ function applyPatches {
     fi
 
     cd $target
+
+    requireCleanWorkTree $target
+
     echo "  Resetting $target to $what..."
     git remote rm upstream 2>/dev/null 2>&1
     git remote add upstream ../../$what >/dev/null 2>&1
-    git checkout master >/dev/null 2>&1
     git fetch upstream >/dev/null 2>&1
     git reset --hard upstream/upstream
 

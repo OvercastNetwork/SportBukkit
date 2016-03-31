d03da48fcf2b484f35d3ea0f9f9e72454aa4f915
Jedediah Smith <jedediah@silencegreys.com>
2016-03-31 05:22:48 -0400

    Update to Minecraft 1.9.2

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-NMS_VERSION="1.9"
+NMS_VERSION="1.9.2"
 NMS_JAR="cache/nms_${NMS_VERSION}.jar"
 NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${NMS_VERSION}/minecraft_server.${NMS_VERSION}.jar"
-NMS_MD5=ff68834eee875bcf367422c67673207c
+NMS_MD5=560d467028f17a70997151ed0053deb6
 

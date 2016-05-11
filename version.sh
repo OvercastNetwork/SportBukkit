dfeb4b1fe0b523c3f79e5bb3c404e79d1b145c73
Jedediah Smith <jedediah@silencegreys.com>
2016-05-11 15:48:35 -0400

    Rebase to Minecraft 1.9.4

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-NMS_VERSION="1.9.2"
+NMS_VERSION="1.9.4"
 NMS_JAR="cache/nms_${NMS_VERSION}.jar"
 NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${NMS_VERSION}/minecraft_server.${NMS_VERSION}.jar"
-NMS_MD5=560d467028f17a70997151ed0053deb6
+NMS_MD5=292feb3085809e714da2ca75514b9d5b
 

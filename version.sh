d12d88462d4c2deb48264f443eca57d74b70f82c
Jedediah Smith <jedediah@silencegreys.com>
2016-06-09 00:32:50 -0400

    Update dependencies to 1.10

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-NMS_VERSION="1.9.4"
+NMS_VERSION="1.10"
 NMS_JAR="cache/nms_${NMS_VERSION}.jar"
 NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${NMS_VERSION}/minecraft_server.${NMS_VERSION}.jar"
-NMS_MD5=292feb3085809e714da2ca75514b9d5b
+NMS_MD5=deb46093d99440d249b6065887403ec8
 

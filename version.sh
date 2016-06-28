5450fbac9603abad3a8a3acf79f5c3e9fd56610b
Jedediah Smith <jedediah@silencegreys.com>
2016-06-27 22:53:31 -0400

    Rebase to Minecraft 1.10.2

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-NMS_VERSION="1.10"
+NMS_VERSION="1.10.2"
 NMS_JAR="cache/nms_${NMS_VERSION}.jar"
 NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${NMS_VERSION}/minecraft_server.${NMS_VERSION}.jar"
-NMS_MD5=deb46093d99440d249b6065887403ec8
+NMS_MD5=41c4e081defd80b09cb0391c894c2f3c
 

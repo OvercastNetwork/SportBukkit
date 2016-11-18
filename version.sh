5914efb00af831ef4ee6cc548621bd73e99502d6
Jedediah Smith <jedediah@silencegreys.com>
2016-11-17 20:22:03 -0500

    Update submodules and version.sh

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
-NMS_VERSION="1.10.2"
+NMS_VERSION="1.11"
 NMS_JAR="cache/nms_${NMS_VERSION}.jar"
 NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${NMS_VERSION}/minecraft_server.${NMS_VERSION}.jar"
-NMS_MD5=41c4e081defd80b09cb0391c894c2f3c
+NMS_MD5=77af7daf2793bf4be81edb623db7cbf3
 
